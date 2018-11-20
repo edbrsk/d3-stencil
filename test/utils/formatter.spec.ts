c1381f0e508d01e6da5048f1b6149d56e17794a9
Edgar Ordóñez <edgar.rodriguez@kiwi.com>
2018-11-20 17:21:24 +0100

    refactor(Added dependencies + Refactor formatter test): Dependencies/paths jest, changes on formatter test

--- test/utils/formatter.spec.ts
+++ test/utils/formatter.spec.ts
@@ -1,5 +1,4 @@
 import { formatter } from './../../src/utils';
-import { Formats } from './../../src/interfaces';
 
 describe('formatter', () => {
   const inputs = {
@@ -10,72 +9,69 @@ describe('formatter', () => {
     shortMonth: 1523442599,
     largeMonth: 1523442599,
     dayAndMonth: 1523442599,
-    any: 'simple text'
+    any: 'simple text',
   };
 
   it('Should return a string formatted like a percentage', () => {
-    const result = formatter(Formats.PERCENTAGE, inputs.percentage);
+    const result = formatter('PERCENTAGE', inputs.percentage);
     expect(result).toEqual('50%');
   });
 
   it('Should return a string grouped with two digits', () => {
-    const result = formatter(
-      Formats.GROUPED_TWO_DIGITS,
-      inputs.groupedTwoDigits
-    );
+    const result = formatter('GROUPED_TWO_DIGITS', inputs.groupedTwoDigits);
 
     expect(result).toEqual('2.6k');
   });
 
   it('Should return a string grouped per thousands with two digits', () => {
     const result = formatter(
-      Formats.GROUPED_THOUSANDS_TWO_DIGITS,
-      inputs.groupedThousandsTwoDigits
+      'GROUPED_THOUSANDS_TWO_DIGITS',
+      inputs.groupedThousandsTwoDigits,
     );
 
     expect(result).toEqual('2,600');
   });
 
   it('Should return the same as input', () => {
-    const result = formatter(Formats.ANY, inputs.any);
+    const result = formatter('ANY', inputs.any);
     expect(result).toEqual(inputs.any);
   });
 
   describe('formatter: Should return a string in currency format specified', () => {
     it('should return a string in EUR format', () => {
-      const result = formatter(Formats.CURRENCY, inputs.currency, 'EUR');
+      const result = formatter('CURRENCY', inputs.currency, 'EUR');
 
-      expect(result).toEqual('€10.00');
+      expect(result).toEqual('€ 10.00');
     });
 
     it('should return a string in USD format', () => {
-      const result = formatter(Formats.CURRENCY, inputs.currency, 'USD');
+      const result = formatter('CURRENCY', inputs.currency, 'USD');
 
-      expect(result).toEqual('$10.00');
+      expect(result).toEqual('US$ 10.00');
     });
 
     it('should return a string in GBP format', () => {
-      const result = formatter(Formats.CURRENCY, inputs.currency, 'GBP');
+      const result = formatter('CURRENCY', inputs.currency, 'GBP');
 
-      expect(result).toEqual('£10.00');
+      expect(result).toEqual('£ 10.00');
     });
   });
 
   describe('formatter: Should return a string in differents formats with timestamp as input', () => {
     it('Should return a string in format short month', () => {
-      const result = formatter(Formats.SHORT_MONTH, inputs.shortMonth);
+      const result = formatter('SHORT_MONTH', inputs.shortMonth);
 
       expect(result).toEqual('Apr');
     });
 
     it('Should return a string in format large month', () => {
-      const result = formatter(Formats.LARGE_MONTH, inputs.shortMonth);
+      const result = formatter('LARGE_MONTH', inputs.shortMonth);
 
       expect(result).toEqual('April');
     });
 
     it('Should return a string in format day and month', () => {
-      const result = formatter(Formats.DAY_AND_MONTH, inputs.shortMonth);
+      const result = formatter('DAY_AND_MONTH', inputs.shortMonth);
 
       expect(result).toEqual('Apr 11');
     });
