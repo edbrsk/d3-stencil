f775f27666e1e82f68f427087acbf0f30f270811
Edgar Ordóñez <edgar.rodriguez@kiwi.com>
2018-11-20 17:21:24 +0100

    refactor(Added dependencies + Refactor formatter test): Dependencies/paths jest, changes on formatter test

--- stencil.config.ts
+++ stencil.config.ts
@@ -3,10 +3,18 @@ import { sass } from '@stencil/sass';
 
 export const config: Config = {
   namespace: 'd3-stencil',
-  outputTargets: [{type: 'dist'}, {type: 'www'}],
+  outputTargets: [{ type: 'dist' }, { type: 'www' }],
   plugins: [
     sass({
-      injectGlobalPaths: ['src/scss/objects.scss']
-    })
-  ]
+      injectGlobalPaths: ['src/scss/objects.scss'],
+    }),
+  ],
+  testing: {
+    moduleNameMapper: {
+      '@d3-stencil/decorators': '<rootDir>/src/decorators/index.ts',
+      '@d3-stencil/interfaces': '<rootDir>/src/interfaces/index.ts',
+      '@d3-stencil/shared': '<rootDir>/src/shared/index.ts',
+      '@d3-stencil/utils': '<rootDir>/src/utils/index.ts',
+    },
+  },
 };
