244d019d7a6c159d9186f26308e69d7eef34b2c5
Edgar Ordóñez <edgar.rodriguez@kiwi.com>
2018-11-25 16:23:09 +0100

    refactor(currency formatter): Added Intl package
    To avoid some errors with the navigator.toLocaleString I added the Intl
    package.
    
    Closes 11

--- src/shared/formats.ts
+++ src/shared/formats.ts
@@ -1,21 +1,24 @@
+import Intl from 'intl';
 import { format } from 'd3-format';
 import { timeFormat } from 'd3-time-format';
 import { Formats } from '@d3-stencil/interfaces';
 
+const currencyFormatter = (data: number, currency: string): string =>
+  new Intl.NumberFormat(undefined, { style: 'currency', currency }).format(
+    data,
+  );
+
 const formats: {
   [format in Formats]: (
     data: string | number,
-    CURRENCY?: string,
+    currency?: string,
   ) => string | number
 } = {
   ['PERCENTAGE']: (data: number) => format('.0%')(data),
   ['GROUPED_TWO_DIGITS']: (data: number) => format('.2s')(data),
   ['GROUPED_THOUSANDS_TWO_DIGITS']: (data: number) => format(',.2r')(data),
-  ['CURRENCY']: (data: number, CURRENCY: string) =>
-    data.toLocaleString(navigator.language, {
-      style: 'currency',
-      currency: CURRENCY,
-    }),
+  ['CURRENCY']: (data: number, currency: string) =>
+    currencyFormatter(data, currency),
   ['SHORT_MONTH']: (data: number) => timeFormat('%b')(new Date(data * 1000)),
   ['LARGE_MONTH']: (data: number) => timeFormat('%B')(new Date(data * 1000)),
   ['DAY_AND_MONTH']: (data: number) =>
