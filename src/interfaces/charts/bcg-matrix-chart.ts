8123c43b6e78131ac7d8631d08c7c3c1a9428d92
Edgar Ordóñez <edgar.rodriguez@kiwi.com>
2019-01-15 13:23:10 +0100

    refactor(interfaces): changes on the interfaces of all the graphs
    NOTICE: This change was overridden.

--- src/interfaces/charts/bcg-matrix-chart.ts
+++ src/interfaces/charts/bcg-matrix-chart.ts
@@ -1,12 +1,14 @@
+import { BcgMatrix } from './../data-types';
 import { Axis, Margin } from './../options-types';
 import { Formats } from './../formats';
 
-export type BcgMatrixChartOptions = Partial<{
-  axis: Axis;
-  quadrants: boolean;
+export interface BcgMatrixChart {
+  data: BcgMatrix[];
+  axis?: Axis;
+  quadrants?: boolean;
+  margin?: Margin;
   value: Partial<{
     format: Formats;
     currency: string;
   }>;
-  margin: Margin;
-}>;
+}
