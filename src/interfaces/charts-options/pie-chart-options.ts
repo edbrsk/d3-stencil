e36432f9180fd7db5f1f93e89bc9903ec221846f
Edgar Ordóñez <edgar.rodriguez@kiwi.com>
2019-01-15 13:23:10 +0100

    refactor(interfaces): changes on the interfaces of all the graphs
    NOTICE: This change was overridden.

--- src/interfaces/charts-options/pie-chart-options.ts
+++
@@ -1,7 +0,0 @@
-import { Formats } from './../formats';
-
-export type PieChartOptions = Partial<{
-  labelFormat: Formats;
-  dataFormat: Formats;
-  currency: string;
-}>;
---
+++ src/interfaces/charts/index.ts
@@ -0,0 +1,7 @@
+import { PieChart } from './pie-chart';
+import { BarChart } from './bar-chart';
+import { LineChart } from './line-chart';
+import { LineAnnotationsChart } from './line-annotations-chart';
+import { BcgMatrixChart } from './bcg-matrix-chart';
+
+export { PieChart, BarChart, LineChart, LineAnnotationsChart, BcgMatrixChart };
