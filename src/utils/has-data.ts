9d3716ebc69b56a2d0263d83ad8e40cf44c59e5f
Edgar Ordóñez <edgar.rodriguez@kiwi.com>
2019-01-18 16:59:34 +0100

    refactor(utils): apply the new interfaces
    Closes #15

--- src/utils/has-data.ts
+++ src/utils/has-data.ts
@@ -1,4 +1,5 @@
 import { GraphData, HasDataStrategy } from '@d3-stencil/interfaces';
+import { BcgMatrix } from '@d3-stencil/interfaces/data-types';
 
 const THROW_ERROR = (): Error => {
   throw new Error(`The data injected isn't valid.`);
@@ -6,16 +7,17 @@ const THROW_ERROR = (): Error => {
 
 export const hasDataIsNotempty: HasDataStrategy = (
   graphData: GraphData,
-  data: any[],
 ): Error | boolean =>
-  data.length > 0 && graphData.labels.length >= 1 ? true : THROW_ERROR();
+  graphData.data.length > 0 && graphData.labels.length >= 1
+    ? true
+    : THROW_ERROR();
 
 export const hasDataValidOnAnnotationsChart: HasDataStrategy = (
   graphData: GraphData,
 ): Error | boolean => {
   if (
     !(
-      graphData.lineChart.data.length > 0 &&
+      graphData.data.length > 0 &&
       graphData.lineAnnotationsChart.increaseHeight > 0 &&
       graphData.lineAnnotationsChart.tickSeparation !== '' &&
       graphData.lineAnnotationsChart.imagePathOneAnnotation !== '' &&
@@ -28,7 +30,6 @@ export const hasDataValidOnAnnotationsChart: HasDataStrategy = (
   return true;
 };
 
-export const hasDataBCGMatrixIsNotEmpty: HasDataStrategy = (
-  graphData: GraphData,
-): Error | boolean =>
-  graphData.bcgMatrixChart.data.length > 0 ? true : THROW_ERROR();
+export const hasDataBCGMatrixIsNotEmpty: HasDataStrategy<BcgMatrix[]> = (
+  graphData: GraphData<BcgMatrix[]>,
+): Error | boolean => (graphData.data.length > 0 ? true : THROW_ERROR());
