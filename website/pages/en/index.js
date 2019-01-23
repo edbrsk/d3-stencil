23512f35d9b6f4d5952bc67de6c915b7c8ca28e3
Edgar Ordóñez <edgar.rodriguez@kiwi.com>
2019-01-23 15:40:46 +0100

    feat(website): created website for docs under docusaurus.io
    Closes #19

---
+++ website/pages/en/index.js
@@ -0,0 +1,147 @@
+const React = require('react');
+
+const CompLibrary = require('../../core/CompLibrary.js');
+
+const MarkdownBlock = CompLibrary.MarkdownBlock; /* Used to read markdown */
+const Container = CompLibrary.Container;
+const GridBlock = CompLibrary.GridBlock;
+
+class HomeSplash extends React.Component {
+  render() {
+    const { siteConfig, language = '' } = this.props;
+    const { baseUrl, docsUrl } = siteConfig;
+    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
+    const langPart = `${language ? `${language}/` : ''}`;
+    const docUrl = doc => `${baseUrl}${docsPart}${langPart}${doc}`;
+    const imgUrl = imgPath => `${baseUrl}${imgPath}`;
+
+    const SplashContainer = props => (
+      <div className="homeContainer">
+        <div className="homeSplashFade">
+          <div className="wrapper homeWrapper">{props.children}</div>
+        </div>
+      </div>
+    );
+
+    const Logo = props => (
+      <div className="projectLogo">
+        <img src={props.img_src} alt="Project Logo" />
+      </div>
+    );
+
+    const ProjectTitle = () => (
+      <h2 className="projectTitle">
+        <small>{siteConfig.tagline}</small>
+      </h2>
+    );
+
+    const PromoSection = props => (
+      <div className="section promoSection">
+        <div className="promoRow">
+          <div className="pluginRowBlock">{props.children}</div>
+        </div>
+      </div>
+    );
+
+    const Button = props => (
+      <div className="pluginWrapper buttonWrapper">
+        <a className="button" href={props.href} target={props.target}>
+          {props.children}
+        </a>
+      </div>
+    );
+
+    const GithubButton = () => (
+      <a
+        className="github-button"
+        href="https://github.com/edgarordonez/d3-stencil"
+        data-icon="octicon-star"
+        data-count-href="/edgarordonez/d3-stencil"
+        data-show-count="true"
+        data-count-api="/repos/edgarordonez/d3-stencil#stargazers_count"
+        data-count-aria-label="# stargazers on GitHub"
+        aria-label="Star edgarordonez/d3-stencil on GitHub"
+      >
+        Star
+      </a>
+    );
+
+    return (
+      <SplashContainer>
+        <div className="inner">
+          <div>
+            <Logo img_src={imgUrl('img/d3-stencil_logo_main.png')} />
+            <ProjectTitle siteConfig={siteConfig} />
+          </div>
+          <PromoSection>
+            <Button href={docUrl('basics/basics-install.html')}>
+              Getting started
+            </Button>
+            <Button href={docUrl('api/api-types.html')}>API Reference</Button>
+          </PromoSection>
+          <GithubButton />
+        </div>
+      </SplashContainer>
+    );
+  }
+}
+
+class Index extends React.Component {
+  render() {
+    const { config: siteConfig, language = '' } = this.props;
+    const { baseUrl } = siteConfig;
+
+    const Block = props => (
+      <Container
+        padding={['bottom', 'top']}
+        id={props.id}
+        background={props.background}
+      >
+        <GridBlock
+          align="center"
+          contents={props.children}
+          layout={props.layout}
+        />
+      </Container>
+    );
+
+    const Features = () => (
+      <Block layout="fourColumn">
+        {[
+          {
+            content:
+              'Chose the right chart. Put your data in d3-stencil, and visualise it.',
+            image: `${baseUrl}img/clock-emoji.png`,
+            imageAlign: 'top',
+            title: 'Simple',
+          },
+          {
+            content:
+              'D3.js is an incredibly successful library, and Stencil, as they said, are just Web Components, so they work with any major framework or no framework at all. Nothing more to say. Powerful.',
+            image: `${baseUrl}img/fire-emoji.png`,
+            imageAlign: 'top',
+            title: 'D3 - Stencil',
+          },
+          {
+            content:
+              'The only limit is your own imagination. If you like what you see, add more graphs.',
+            image: `${baseUrl}img/puzzle-emoji.png`,
+            imageAlign: 'top',
+            title: 'Extensible',
+          },
+        ]}
+      </Block>
+    );
+
+    return (
+      <div>
+        <HomeSplash siteConfig={siteConfig} language={language} />
+        <div className="mainContainer">
+          <Features />
+        </div>
+      </div>
+    );
+  }
+}
+
+module.exports = Index;
---
+++ website/siteConfig.js
@@ -0,0 +1,78 @@
+// List of projects/orgs using your project for the users page.
+const users = [
+  {
+    caption: 'User1',
+    // You will need to prepend the image path with your baseUrl
+    // if it is not '/', like: '/test-site/img/docusaurus.svg'.
+    image: '/img/docusaurus.svg',
+    infoLink: 'https://www.facebook.com',
+    pinned: true,
+  },
+];
+
+const siteConfig = {
+  title: 'D3-Stencil',
+  tagline: 'Framework-agnostic, simple.',
+  url: 'https://edgarordonez.github.io', // Your website URL
+  baseUrl: '/', // Base URL for your project */
+  // For github.io type URLs, you would set the url and baseUrl like:
+  //   url: 'https://facebook.github.io',
+  //   baseUrl: '/test-site/',
+
+  // Used for publishing and more
+  projectName: 'D3-Stencil',
+  organizationName: 'Edgar Ordóñez',
+
+  // For no header links in the top nav bar -> headerLinks: [],
+  headerLinks: [
+    { doc: 'basics/basics-install', label: 'Getting Started' },
+    { doc: 'api/api-types', label: 'API Reference' },
+    { page: 'help', label: 'Help' },
+    { blog: false },
+  ],
+
+  // If you have users set above, you add it here:
+  // users,
+
+  /* path to images for header/footer */
+  headerIcon: 'img/d3-stencil_logo.png',
+  footerIcon: 'img/d3-stencil_footer.png',
+  favicon: 'img/favicon.png',
+
+  disableHeaderTitle: true,
+  disableTitleTagline: false,
+
+  /* Colors for website */
+  colors: {
+    primaryColor: '#2c4c5c',
+    secondaryColor: '#e48e21',
+  },
+
+  // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
+  copyright: `Copyright © ${new Date().getFullYear()} D3-Stencil`,
+
+  highlight: {
+    // Highlight.js theme to use for syntax highlighting in code blocks.
+    theme: 'atom-one-dark',
+    defaultLang: 'typescript',
+  },
+
+  // Add custom scripts here that would be placed in <script> tags.
+  scripts: ['https://buttons.github.io/buttons.js'],
+
+  // On page navigation for the current documentation page.
+  onPageNav: 'separate',
+
+  // No .html extensions for paths.
+  cleanUrl: true,
+
+  // Open Graph and Twitter card images.
+  ogImage: 'img/d3-stencil_logo.png',
+  twitterImage: 'img/d3-stencil_logo.png',
+
+  // You may provide arbitrary config keys to be used as needed by your
+  // template. For example, if you need your repo's URL...
+  repoUrl: 'https://github.com/edgarordonez/d3-stencil',
+};
+
+module.exports = siteConfig;
