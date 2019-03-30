8670a06f353b5862afedea596c419fc243a08a17
Edgar Ordóñez <edgar.rodriguez@kiwi.com>
2019-03-30 11:29:07 +0100

    docs(website): added google analytics to track visits

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -70,6 +70,10 @@ const siteConfig = {
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
   repoUrl: 'https://github.com/edgarordonez/d3-stencil',
+
+  // Google Analytics tracking ID to track page views.
+  gaTrackingId: 'UA-137205216-1',
+  gaGtag: true,
 };
 
 module.exports = siteConfig;
